2595fde98b50964c0c0b7cb3a7300b7351fb0a4f
aleluye <36299642+aleluye@users.noreply.github.com>
2019-04-11 22:35:03 +0800

    Add files via upload

---
+++ ys/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div id="nav">
+        <Header></Header>
+        <router-link to="/">Home</router-link>
+        <router-link to="/contactUs">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+import Header from '@/views/header/header.vue'
+export default {
+    components: {
+        Header
+    }
+}
+</script>
+<style lang="less">
+a {
+    text-decoration: none;
+    out-line: none;
+    color: #fff;
+}
+</style>
